a5b037b894078b06eaab639eb05d56a70790ea8c
halldor03 <kacoleker@gmail.com>
2022-10-14 00:13:41 +0200

    Started working on sidebar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const tasks = document.querySelectorAll(".task");
+tasks.forEach((tasks) => {
+  tasks.addEventListener("click", () => {
+    tasks.classList.add("task_active");
+  });
+});
+
+const inputContainer = document.getElementById("tasks");
+const input = document.getElementById("addNewTask");
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    console.log("Enter key pressed!!!!!");
+    const newTask = document.createElement("div");
+    newTask.classList.add("task");
+    newTask.innerText = input.value;
+    inputContainer.appendChild(newTask);
+  }
+});
